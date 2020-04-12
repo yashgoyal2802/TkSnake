bee63b76852768d1fdbf00ab409e50698f827ae8
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-12 23:17:38 +0530

    loading asssets
    getting the image

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from tkinter import *
+from PIL import Image, ImageTk
 
 
 class Snake(Canvas):
     def __init__(self):
         super().__init__(width=600, height=620, background="black", highlightthickness=0)
+
+        self.load_assets()
+
+    def load_assets(self):
+        try:
+            # Snake image
+            self.snake_body_image = Image.open("./assets/snake.png")
+            self.snake_body = ImageTk.PhotoImage(self.snake_body_image)
+            # Food Image
+            self.food_image = Image.open("./assets/food.png")
+            self.food = ImageTk.PhotoImage(self.food_image)
+        except IOError as error:
+            print(error)
+            root.destroy()
 
 
 root = Tk()
