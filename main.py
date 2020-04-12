fd9f13ef90741d2e8bc09df3ec416f1e9c433d24
BlackHatGhost <yashdineshgoyal@gmail.com>
2020-04-12 22:39:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from tkinter import *
 
+
+class Snake(Canvas):
+    def __init__(self):
+        super().__init__(width=600, height=620, background="black", highlightthickness=0)
+
+
 root = Tk()
 root.title("Snake Game!!")
 root.resizable(False, False)    # Making it un-resizable
 
+python = Snake()
+python.pack()
 
 root.mainloop()
 
